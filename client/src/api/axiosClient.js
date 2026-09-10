2ea41103bbde0d7f0ec1e9b4e52dbb79f756a117
Omoniyi47 <emmanuelwemida@student.oauife.edu.ng>
2026-09-10 14:25:03 +0100

    refactor: simplify API base URL assignment

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = (
-  import.meta.env.VITE_API_BASE_URL || 'https://five08-group-project.onrender.com/api'
-).replace(/\/+$/, '');
+const API_BASE_URL = 'https://five08-group-project.onrender.com/api';
 
 let accessToken = null;
 let onAuthFailure = null;
